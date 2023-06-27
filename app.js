e81248a37b758c077e0d3f56060b40e49a5e47aa
javadng <javadnasirifu@gmail.com>
2023-06-27 20:29:22 +0330

    upload

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const config = require("./utils/config");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const socket = require("./socket");
+const connectToDB = require("./utils/mongodb");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: config.crosOrigin,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  },
+});
+
+app.use(cookieParser());
+
+app.get("/api", (_, res) => res.send(`Server is running.`));
+
+httpServer.listen(config.port, config.host, () => {
+  console.log(`server is runnig 🚀 on port: ${config.port}`);
+  console.log(`> http://${config.host}:${config.port}`);
+
+  connectToDB().catch((err) => console.log(`error: ${err}`));
+
+  socket(io);
+});
---
+++ socket.js
@@ -0,0 +1,18 @@
+const { parse } = require("cookie");
+const authController = require("./Controllers/authController");
+const chatController = require("./Controllers/chatController");
+const roomsController = require("./Controllers/roomsController");
+const EVENTS = require("./utils/events");
+const jwt = require("jsonwebtoken");
+
+const socket = (io) => {
+  io.on(EVENTS.connection, (socket) => {
+    console.log("new connection " + Math.random());
+
+    authController(io, socket);
+    roomsController(io, socket);
+    chatController(io, socket);
+  });
+};
+
+module.exports = socket;
