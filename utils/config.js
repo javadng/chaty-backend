14da88c8b71fc21575e6a88f62d8195dda51a139
javadng <javadnasirifu@gmail.com>
2026-02-08 19:32:27 +0330

    master: cors bug fixed

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   corsOriginLocal: "http://localhost:3000",
-  corsOriginOnline: "https://chaty-front-web.vercel.app",
+  corsOriginOnline: "https://chaty-front.vercel.app",
   port: 3001,
   host: "https://chaty-server-sandy.vercel.app/",
   // host: "localhost",
